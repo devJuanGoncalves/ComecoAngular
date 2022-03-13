26ab72d26fedb2522a970a72aa6f152a6aaf7271
DevJuanGoncalves <juan.goncalves.dev@gmail.com>
2022-03-13 18:10:54 -0300

    Exportando dados com output

--- my-first-project/src/app/nova-transferencia/nova-transferencia.component.ts
+++ my-first-project/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,10 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number;
   destino: number;
 
   transferir() {
     console.log('Solicitada nova transferencia');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
   }
 }
