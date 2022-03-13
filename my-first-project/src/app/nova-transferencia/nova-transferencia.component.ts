840c1560180bcb0389d803e6b846ddb5f0bf35e7
DevJuanGoncalves <juan.goncalves.dev@gmail.com>
2022-03-13 17:52:27 -0300

    Property bind

--- my-first-project/src/app/nova-transferencia/nova-transferencia.component.ts
+++ my-first-project/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
+
   transferir() {
     console.log('Solicitada nova transferencia');
   }
