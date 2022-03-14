5258c952077b4259f60dbe8d1ca12727c7d9f46f
DevJuanGoncalves <juan.goncalves.dev@gmail.com>
2022-03-14 02:26:05 -0300

    Criando o primeiro service

--- my-first-project/src/app/extrato/extrato.component.ts
+++ my-first-project/src/app/extrato/extrato.component.ts
@@ -1,16 +1,17 @@
+import { TransferenciaService } from './../service/transferencia.service';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
-  styleUrls: ['./extrato.component.scss']
+  styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
-  @Input() transferencias: any[] = [];
+  transferencias: any[] = [];
 
-  constructor() { }
+  constructor(private service: TransferenciaService) {}
 
   ngOnInit(): void {
+    this.transferencias = this.service.transferencias;
   }
-
 }
